d841deafafcd5959e632fcff4913aa58fdedefbb
PRC <105520102+nocaprc@users.noreply.github.com>
2024-08-09 10:48:41 +0530

    sdsd

---
+++ C++/Uni/U1/typeCasting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	int intvar = 25;
+	float floatvar = 35.87;
+
+	cout<<"intvar = "<<intvar;
+	cout<<"\nfloat(intvar) = "<< float(intvar);
+
+	getch();
+}
