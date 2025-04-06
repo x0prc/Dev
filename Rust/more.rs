ef9a1af384cafb6f616219e043ed2c1925bc3814
x0prc <c0ldheat@protonmail.com>
2025-04-06 22:51:47 +0530

    feat: struct and associated

--- Rust/more.rs
+++ Rust/more.rs
@@ -10,3 +10,20 @@ fn first(s: &String)-> usize {
 }
 let s = String::from("hello world");
 let hello = &s[0..5];
+assert_eq!(slice, &[2, 3]);
+
+//struct
+#[derive(Deserialize, Debug)]
+struct User {
+    active: bool,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn new(){
+    let user1 = User {
+        active: true,
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+}
