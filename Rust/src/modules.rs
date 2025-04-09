9f9b1dffc56b8343c8b8ef124b45ba1948b952de
x0prc <c0ldheat@protonmail.com>
2025-04-09 20:30:32 +0530

    feat: add src directory and toml config

---
+++ Rust/src/modules.rs
@@ -0,0 +1,11 @@
+pub mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+        pub fn seat_at_table() {}
+    }
+    pub mod serving {
+        pub fn take_order() {}
+        pub fn serve_order() {}
+        pub fn take_payment() {}
+    }
+}
