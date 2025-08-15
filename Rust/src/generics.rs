f29dd508d72dafffb6f39848506e312e4f7178c7
PRC <105520102+x0prc@users.noreply.github.com>
2025-08-15 22:54:17 +0530

    feat : impl generics

--- Rust/src/generics.rs
+++ Rust/src/generics.rs
@@ -13,3 +13,64 @@ fn bar<T, const M: usize>() {
     func::<M>();
     let _: [u8, M];
 }
+
+struct A;          // Concrete type `A`.
+struct S(A);       // Concrete type `S`.
+struct SGen<T>(T); // Generic type `SGen`.
+
+fn reg_fn(_s: S) {}
+
+fn gen_spec_t(_s: SGen<A>) {}
+
+fn gen_spec_i32(_s: SGen<i32>) {}
+
+fn generic<T>(_s: SGen<T>) {}
+
+fn main() {
+    // Using the non-generic functions
+    reg_fn(S(A));          // Concrete type.
+    gen_spec_t(SGen(A));   // Implicitly specified type parameter `A`.
+    gen_spec_i32(SGen(7)); // Implicitly specified type parameter `i32`.
+
+    // Explicitly specified type parameter `char` to `generic()`.
+    generic::<char>(SGen('A'));
+
+    // Implicitly specified type parameter `char` to `generic()`.
+    generic(SGen(7.7));
+
+    println!("Success!");
+}
+
+// generic for val
+struct Val<T> {
+    val: T,
+}
+
+impl<T> Val<T> {
+    fn value(&self) -> &T {
+        &self.val
+    }
+}
+
+fn main() {
+    let x: Val<f64> = Val{ val: 3.0 };
+    let y: Val<String> = Val{ val: "hello".to_string()};
+    println!("{}, {}", x.value(), y.value());
+}
+
+// generics
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl Point<f64> {
+    fn distance_from_origin(&self) -> f64 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+fn main() {
+    let p = Point{x: 5.0, y: 10.0};
+    println!("{}",p.distance_from_origin());
+}
