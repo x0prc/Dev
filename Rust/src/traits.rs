5ab921aa8dc1ff67bd4410f1f351a17fa045057e
PRC <105520102+x0prc@users.noreply.github.com>
2025-08-18 22:50:46 +0530

    feat (rust) : learn more on strings + adv traits

--- Rust/src/traits.rs
+++ Rust/src/traits.rs
@@ -139,3 +139,97 @@ fn main() {
 
     println!("One foot is {} than one meter.", cmp);
 }
+
+// Box <dyn> usage
+
+trait Bird {
+    fn quack(&self) -> String;
+}
+
+struct Duck;
+impl Duck {
+    fn swim(&self) {
+        println!("Look, the duck is swimming")
+    }
+}
+struct Swan;
+impl Swan {
+    fn fly(&self) {
+        println!("Look, the duck.. oh sorry, the swan is flying")
+    }
+}
+
+impl Bird for Duck {
+    fn quack(&self) -> String{
+        "duck duck".to_string()
+    }
+}
+
+impl Bird for Swan {
+    fn quack(&self) -> String{
+        "swan swan".to_string()
+    }
+}
+
+fn main() {
+    // FILL in the blank.
+    let duck : Duck = Duck;
+    duck.swim();
+
+    let bird: Box<dyn Bird> = hatch_a_bird(2);
+    // This bird has forgotten how to swim, so below line will cause an error.
+    // bird.swim();
+    // But it can quak.
+    assert_eq!(bird.quack(), "duck duck");
+
+    let bird: Box<dyn Bird> = hatch_a_bird(1);
+    // This bird has forgotten how to fly, so below line will cause an error.
+    // bird.fly();
+    // But it can quak too.
+    assert_eq!(bird.quack(), "swan swan");
+
+    println!("Success!");
+}   
+
+// IMPLEMENT this function.
+fn hatch_a_bird(species : u8) -> Box<dyn Bird> {
+    match species {
+        1 => Box::new(Swan),
+        2 => Box::new(Duck),
+        _ => panic!(),
+    }
+}
+
+// static and dynamic dispatch
+trait Foo {
+    fn method(&self) -> String;
+}
+
+impl Foo for u8 {
+    fn method(&self) -> String { format!("u8: {}", *self) }
+}
+
+impl Foo for String {
+    fn method(&self) -> String { format!("string: {}", *self) }
+}
+
+// IMPLEMENT below with generics.
+fn static_dispatch<T: Foo>(a: T) {
+    a.method();
+}
+
+// Implement below with trait objects.
+fn dynamic_dispatch(a: &dyn Foo) {
+    a.method();
+}
+
+fn main() {
+    let x: u8 = 5u8;
+    let y: String = "Hello".to_string();
+
+    static_dispatch(x);
+    dynamic_dispatch(&y);
+
+    println!("Success!");
+}
+
