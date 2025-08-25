a001033cbd6bef0fade75aec8deb0b9624a85918
PRC <105520102+x0prc@users.noreply.github.com>
2025-08-25 13:01:59 +0530

    feat (rust) : more ? usage

--- Rust/src/result.rs
+++ Rust/src/result.rs
@@ -18,3 +18,51 @@ fn main() {
 
     println!("Success!");
 }
+
+use std::fs::File;
+use std::io::{self, Read};
+
+fn read_file1() -> Result<String, io::Error> {
+    let f: Result<File, io:Error> = File::open("hello.txt");
+    let mut f: File = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s: String = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+// FILL in the blanks with one code line
+// DON'T change any code lines
+fn read_file2() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
+fn main() {
+    assert_eq!(read_file1().unwrap_err().to_string(), read_file2().unwrap_err().to_string());
+    println!("Success!");
+}
+
+use std::num::ParseIntError;
+
+// IMPLEMENT multiply with ?
+// DON'T use unwrap here
+fn multiply(n1_str: &str, n2_str: &str) -> Result<i32, ParseIntError> {
+    let n1: i32 = n1_str.parse::<i32>()?;
+    let n2: i32 = n2_str.parse::<i32>()?;
+    
+    Ok(n1 * n2)
+}
+
+fn main() {
+    assert_eq!(multiply("3", "4").unwrap(), 12);
+    println!("Success!");
+}
