e19ec7a6f15c16252973656015a59d2f85bcfb30
PRC <105520102+x0prc@users.noreply.github.com>
2025-08-26 19:12:10 +0530

    rust : impl mods

--- Rust/src/modules.rs
+++ Rust/src/modules.rs
@@ -9,3 +9,18 @@ pub mod front_of_house {
         pub fn take_payment() {}
     }
 }
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::front_of_house::serving::serve_order();
+    }
+
+    fn cook_order() {}
+}
+
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    front_of_house::hosting::add_to_waitlist();
+}
